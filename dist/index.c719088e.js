394ddf69b8baf4c7c7833fd54da9836ea813c91a
C <cuong.nguyenmanh@eightcap.com>
2022-02-23 18:14:43 +0700

    wed 3

--- dist/index.c719088e.js
+++ dist/index.c719088e.js
@@ -1,13 +1,19 @@
 const box = document.querySelector('.company__strength');
 const zoomElement = document.querySelector('.company__title');
+const zoomElementContainer = document.querySelector('#container');
+const zoomPoint = document.querySelector('.zoom__point');
 const textSlideInFromRight = document.querySelector('.text__1');
 const textSlideInFromLeft = document.querySelector('.text__2');
 let zoomOriginalLeft = 70;
 let zoomOriginalRight = -70;
-let slideSpeed = 1;
-let originalZoom = 0;
-let zoomSpeed = 0.9;
-document.addEventListener('scroll', function(e) {
+let slideSpeed = 0.7;
+let originalZoom = 4;
+let zoomSpeed = 0.1;
+let zoomAddition = 10;
+document.addEventListener('scroll', function(e, scale = 1, fit = false) {
+    let body = document.body;
+    console.log(window.innerHeight);
+    const windownHeight = window.innerHeight;
     // let boxProp = box.getBoundingClientRect();
     // const isInViewOrNot = boxProp.top >= 0 && 
     // boxProp.left >= 0 && 
@@ -15,7 +21,9 @@ document.addEventListener('scroll', function(e) {
     // boxProp.right <= (window.innerWidth || document.documentElement.clientWidth)
     // console.log(isInViewOrNot)
     zoomElement.classList.add('transform__origin');
-    zoomElement.style.transform = `scale(  ${10 + (originalZoom = originalZoom + zoomSpeed)},  ${10 + (originalZoom = originalZoom + zoomSpeed)})`;
+    // zoomElement.style.paddingLeft = `${zoomAddition += 0.1}px`;
+    console.log(zoomElement.style.paddingLeft);
+    zoomElement.style.transform = `scale(  ${zoomAddition + (originalZoom = originalZoom + zoomSpeed)},  ${zoomAddition + (originalZoom = originalZoom + zoomSpeed)})`;
     textSlideInFromLeft.style.transform = `translate(${zoomOriginalLeft = zoomOriginalLeft - slideSpeed}%, 0px)`;
     textSlideInFromRight.style.transform = `translate(${zoomOriginalRight = zoomOriginalRight + slideSpeed}%, 0px)`;
 });
